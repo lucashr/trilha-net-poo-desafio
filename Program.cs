759eda50c8b41e07a334672bd5ff404dad0aa3db
lucashr <lucas_rdasilva@hotmail.com>
2024-09-06 22:39:58 -0300

    finalização do desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia nokia = new Nokia("123456", "NK47", "00323146781", 1000);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("AppFinancasXpto");
+
+Console.WriteLine();
+
+Iphone iphone = new Iphone("789456", "IFONE363", "0066811743", 3250);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("IJogosXpto");
